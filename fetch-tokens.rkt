51c1bb7b12b68f6a2452f28cdd550b70e4e39fee
Lei Wang <LeiWang2013@u.northwestern.edu>
2013-07-16 09:52:22 -0500

    simple test case created, listed final goal

--- fetch-tokens.rkt
+++ fetch-tokens.rkt
@@ -14,16 +14,33 @@
 (read-line sp)
 (read-line sp)
 
-
 (get-them (open-input-string "@f{x}"))
+|#
+
+#|simple result
+1)function "tab adjusted"
+
+@itemlist[@item{item1}
+          @item{item2}]
+
+2)String "one-space adjusted"
+
+@centered{
+ @bold{Cookies Wanted}
+ @italic{Chocolate chip preferred!}
+}
 |#
 
 (require framework)
 
 (define txt (new racket:text%))
-(send txt insert "#lang scribble/base\n@f{x}")
-(for/list ([x (in-range (send txt last-position))])
-  (send txt classify-position x))
+(send txt insert "#lang scribble/base\n@itemlist[@item{item1}\n
+                                @item{item2");#lang scribble/base\n@f{x}")
+
+;;first basic position classify method
+(define (txt-position-classify txt)
+  (for/list ([x (in-range (send txt last-position))])
+    (send txt classify-position x)))
 
 (send txt get-token-range 0)
 
@@ -31,10 +48,20 @@
 
 (define txt2 (new racket:text%))
 (send txt2 insert "#lang scribble/base\n@f{\n x\n}")
+#|should be:
+
+#lang scribble/base
+@f{
+ x
+}
+
+after adjusted|#
 
 (define txt3 (new racket:text%))
 (send txt3 insert "#lang scribble/base\n@f[\n x\n]")
 
+(define (space-filter-inserter txt) 
+  txt)
 ;; position-line, 
 
 ;; backward-match, backward-containing-sexp, forward-match
@@ -44,3 +71,10 @@
 ; position-paragraph (method of text%)  provided from racket/gui/base, racket/gui
 ; paragraph-end-position (method of text%)  provided from racket/gui/base, racket/gui
 ; paragraph-start-position (method of text%)  provided from racket/gui/base, racket/gui
+
+
+;;test cases
+(require plai)
+(module+ test
+  ;;first test: able to process string correctly
+  (test (txt-position-classify (space-filter-inserter txt2))  '(other other other other other other other other other other other other other other other other other other other parenthesis symbol parenthesis white-space string parenthesis)))
