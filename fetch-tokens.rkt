5425549725a27578888d8577db7f72e32f858e89
Lei Wang <LeiWang2013@u.northwestern.edu>
2013-07-22 09:17:38 -0500

    more stuff for trying

--- fetch-tokens.rkt
+++ fetch-tokens.rkt
@@ -18,16 +18,6 @@
  space : (and/c real? (not/c negative?))
 |#
 (require framework)
-;;txt used for testing
-(define txt (new racket:text%))
-(send txt insert "#lang scribble/base\n@itemlist[@item{item1}\n
-                                @item{item2}\n]");#lang scribble/base\n@f{x}")
-(define txt2 (new racket:text%))
-(send txt2 insert "#lang scribble/base\n@f{\n x\n}")
-
-(define txt3 (new racket:text%))
-(send txt3 insert "#lang scribble/base\n@f[\n x\n]\n")
-
 
 ;;first basic position classify method: text -> position classify of the whole text
 (define (txt-position-classify txt)
@@ -42,26 +32,20 @@
               (equal? end (+ posi 1))))
        (equal? #\@ (send txt get-character posi))))
 
-;;is-parenthesis: check if given position is a parenthesis
-(define (is-parenthesis txt posi)
-  (if (equal? (send txt classify-position posi) 'parenthesis)
-      (not (equal? (send txt classify-position (add1 posi)) 'symbol))
-      #f))
 ;;determine-spaces : text position[natural] -> spaces @ front or #f
 (define (determine-spaces txt posi)
   (let* ([current_para (send txt position-paragraph posi)]
          [para_start (send txt paragraph-start-position current_para)]
          [para_start_skip_space (send txt skip-whitespace para_start 'forward #t)];skip comment also
-         [current_space (- para_start_skip_space para_start)];; current already have space
+         ;[current_space (- para_start_skip_space para_start)];; current already have space
          [para_end (send txt paragraph-end-position current_para)])
     (if (is-at-sign? txt para_start_skip_space);if the biginning is not an @, just return #f
-        (- 1 current_space)
+        1
         #f)))
-;(send txt get-token-range 0)
 
 ;;next step
-(define (space-filter-inserter txt) 
-  txt)
+;(define (space-filter-inserter txt) 
+;  txt)
 
 ;;;usage instructions
 ;; position-line, given: a position start from 0, return line number it is at
@@ -73,8 +57,9 @@
 
 
 ;; position-paragraph (method of text%)  provided from racket/gui/base, racket/gui
-;; given a position, return what line it is at
+;; given a position, return what paragraph it is at
 ;(send txt2 position-paragraph 24)
+
 ;; paragraph-end-position (method of text%)  provided from racket/gui/base, racket/gui
 ;; return the position before invisible item(newline)
 ;(send txt2 paragraph-end-position 0)
@@ -84,7 +69,16 @@
 
 (module+ test
   (require rackunit)
-  
+  ;;test 1, to be delete
+  (check-equal? (let ([t (new racket:text%)])
+                  (send t insert "#lang scribble/base\n@foo{bar @baz[2 me]{4 5}}")
+                  (txt-position-classify t))
+                (list))
+  ;;test 2, to be delete
+  (check-equal? (let ([t (new racket:text%)])
+                  (send t insert "#lang scribble/base\nWho am I@foo{}")
+                  (txt-position-classify t))
+                (list))
   (define txt_1 (new racket:text%))
   (send txt_1 insert "#lang scribble/base\n@f{x}")
   (define txt_2 (new racket:text%))
@@ -94,18 +88,13 @@
   (define txt_4 (new racket:text%))
   (send txt_4 insert "#lang scribble/base\n@itemlist[@item{item1}\n@item{item2}\n]")
   
-  (check-equal? (send txt_4 backward-match 35 0) 31) ;;not sure about "cutoff"
-  (check-equal? (send txt_4 forward-match 29 100) 57) ;;not working properly on different lines? 
-  (check-equal? (send txt_2 forward-match 22 0) #f) 
-  (check-equal? (send txt_4 forward-match 31 0) 35)
-  (check-equal? (send txt_4 forward-match 43 0) 44) ;; the whole thing is a string????
-  (check-equal? (send txt_4 backward-containing-sexp 29 0) 0);;start from #lang???
-  (check-equal? (send txt_4 backward-containing-sexp 35 0) 30);;@item{item1}
-  ;test is-at-sign is-parenthesis
+  ;test is-at-sign
+  (check-equal? (let ([t (new racket:text%)])
+                  (send t insert "(x)")
+                  (is-at-sign? t 0))
+                #f)
   (check-equal? (is-at-sign? txt_1 20) #t)
   (check-equal? (is-at-sign? txt_1 22) #f)
-  (check-equal? (is-parenthesis txt_1 22) #t)
-  (check-equal? (is-parenthesis txt_1 23) #f)
   ;test skip-white-space
   (check-equal? (send txt_2 skip-whitespace 23 'forward #t) 25)
   ;test determine-spaces
@@ -116,14 +105,21 @@
   (check-equal? (determine-spaces txt_4 22) #f)
   (check-equal? (determine-spaces txt_4 43) 10)
   
-;;first test: able to process string correctly
-#|(check-equal? (txt-position-classify (space-filter-inserter txt2))  
+  ;;first test: able to process string correctly
+  #|(check-equal? (txt-position-classify (space-filter-inserter txt2))  
         '(other other other other other other other other other other 
                 other other other other other other other other other ;;19 other, represents #lang...
                 parenthesis symbol parenthesis white-space string parenthesis))|#
+  #|
+  (check-equal? (send txt_4 backward-match 35 0) 31) ;;not sure about "cutoff"
+  (check-equal? (send txt_4 forward-match 29 100) 57) 
+  (check-equal? (send txt_2 forward-match 22 0) #f) 
+  (check-equal? (send txt_4 forward-match 31 0) 35)
+  (check-equal? (send txt_4 forward-match 43 0) 44) 
+  (check-equal? (send txt_4 backward-containing-sexp 29 0) 0);;start from #lang???|#
+  ;(check-equal? (send txt_4 backward-containing-sexp 35 0) 30);;@item{item1}
+  ;(check-equal? (send txt_4 backward-containing-sexp 29 100) 20);;[@item...
+  ;(check-equal? (let-values ([(start end)(send txt_1 get-token-range 22)]) end) 23);based on paragraph
   
-  
-  (check-equal? (let ([t (new racket:text%)])
-                  (send t insert "(x)")
-                  (is-at-sign? t 0))
-                #f))
+  ;(check-equal? (send txt_1 forward-match 22 100) 25) 
+  )
--- syntax-analysis.rkt
+++ syntax-analysis.rkt
@@ -1,11 +1,46 @@
 #lang at-exp racket
 
-'@foo{blah blah blah}
+'@foo{1 2 3}
 
 (define (foo str) (printf "He wrote ~s.\n" str))
   @foo{blah blah blah}
   
 '@foo[1 2]{3 4}
-	
+
+'@foo[1 2]{3 4}
+
 '@foo{bar @baz{3}
      blah}
+
+	
+'@foo{(+ 1 2) -> @(+ 1 2)!}
+
+'@foo{A @"string" escape}
+
+'@foo{A @"{" begins a block}
+
+'(define \@email
+   "foo@bar.com")
+
+;;spaces are not allowed before a [ or a { !
+'@foo{bar @baz[2 3] {4 5}}
+'@foo{bar @baz[2 3]{4 5}}
+
+;`
+'@(lambda (x) x){blah}
+	
+'@`(unquote foo){blah}
+
+'@`(unquote foo)
+
+'@foo{{{}}{}}
+;;there are currently no special rules for using @ in the command itself, which can lead to things like:
+
+  
+'@@foo{bar}{baz}
+  ;reads as  
+;((foo "bar") "baz")
+
+'@foo|{"}" follows "{"}|
+	
+'@foo{@"}" follows @"{"}
